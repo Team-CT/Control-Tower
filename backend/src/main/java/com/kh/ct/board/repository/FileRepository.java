28e42970dd1f74f1352dba7885c50acb24751f68
yoo-j-h <gusdlcjstk10@gmail.com>
2026-01-30 11:50:01 +0900

    fix : 보드 폴더 삭제 #93

--- backend/src/main/java/com/kh/ct/board/repository/FileRepository.java
+++
@@ -1,6 +0,0 @@
-package com.kh.ct.board.repository;
-
-import com.kh.ct.global.entity.File;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<File, Long> {}
