e9e3143e8a1240f7b44e17f0cfc81364858c1968
yoo-j-h <gusdlcjstk10@gmail.com>
2026-02-02 17:39:06 +0900

    fix : 파일 서비스, 컨트롤러 위치및 내용수정 #93

--- backend/src/main/java/com/kh/ct/domain/board/controller/FileController.java
+++ backend/src/main/java/com/kh/ct/domain/board/controller/FileController.java
@@ -9,15 +9,15 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriUtils;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/file")
@@ -26,15 +26,23 @@ public class FileController {
 
     private final FileService fileService;
 
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Map<String, Long>> upload(
+            @RequestPart("file") MultipartFile file
+    ) throws IOException {
+
+        File saved = fileService.saveFile(file);
+        return ResponseEntity.ok(Map.of("fileId", saved.getFileId()));
+    }
+
+
     @GetMapping("/download/{fileId}")
     public ResponseEntity<Resource> downloadFile(@PathVariable Long fileId) {
         try {
             // 1. DB에서 파일 정보 조회 (실제 경로, 원본 파일명 등)
             // fileService.getFileEntity(fileId) 같은 메서드가 필요합니다.
             File fileEntity = fileService.getFile(fileId);
-
-            Path path = Paths.get(fileEntity.getPath());
-            Resource resource = new UrlResource(path.toUri());
+            Resource resource =  fileService.loadAsResource(fileId);
 
             // 2. 파일명이 한글일 경우 깨짐 방지 처리
             String encodedFileName = UriUtils.encode(fileEntity.getFileOriName(), StandardCharsets.UTF_8);
