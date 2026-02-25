ff98559288fb2e4e45d09b286ebc973e3fe192b5
ziukim <jiwoogim@gmail.com>
2026-02-25 17:12:07 +0900

    fix: 메인페이지 테마관련 403에러 수정
    # Conflicts:
    #	backend/src/main/java/com/kh/ct/global/config/SecurityConfig.java

--- frontend/src/pages/ServiceRegistration/ServiceRegistration.jsx
+++ frontend/src/pages/ServiceRegistration/ServiceRegistration.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useTheme } from 'styled-components';
 import {
   MainContainer,
   ContentWrapper,
